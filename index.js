ebf7a0bf79ef5b18d615c2994e8c00fc3d04d7f0
carlos molero <cmolero130@gmail.com>
2020-10-21 16:51:03 -0400

    trying to get 200ok from server phase2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csurf = require("csurf");
+const csurfProtection = csurf({ cookie: true });
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cookieParser());
 
 app.set("view engine", "pug");
-
-app.get("/", (req, res) => {
-    res.render("index", { users });
-});
 
 const users = [
     {
@@ -17,7 +17,17 @@ const users = [
         email: "jill.jack@gmail.com",
     },
 ];
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/", (req, res) => {
+    res.render("index", { users });
+});
+
+app.get("/create", csurfProtection, (req, res) => {
+    res.render("create", { csrfToken: req.csrfToken() });
+});
+
+
+
 
 module.exports = app;
